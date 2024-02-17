bc6c7c658396020c9c071882344d15120a14942d
Arnold Jiang <aj9183@princeton.edu>
2024-02-16 21:05:07 -0500

    formula - bad

--- Student_aj9183.java
+++ Student_aj9183.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 
 public class Student_aj9183 implements Student {
     private class School implements Comparable<School> {
-        public School(int i, double q) {
+        public School(int i, double s, double q) {
           index = i;
+          synergy = s;
           quality = q;
         }
     
         private int index;
+        private double synergy;
         private double quality;
     
         public int compareTo(School n) { // smaller pairs are higher quality
@@ -27,25 +29,26 @@ public class Student_aj9183 implements Student {
         List<Double> synergies) {
 
         // Creates a list of preferences for each school
-        School[] topSynergy = new School[schools.size()];
+        School[] synergy = new School[schools.size()];
+        School[] preferences = new School[25];
 
-        // Creates a list of schools and sorts them based on synergies
-        for (int i = 0; i != synergies.size(); ++i) {
-          topSynergy[i] = new School(i, synergies.get(i));
+        // sorts the schools based on synergies
+        for (int i = 0; i != schools.size(); ++i) {
+          synergy[i] = new School(i, synergies.get(i), schools.get(i));
         }
-        Arrays.sort(topSynergy);
+        Arrays.sort(synergy);
 
-        // Ranking System of Aptitude and Quality of Schools
-        for (int i = 0; i != topSynergy.length; ++i) {
-          double rank = Math.abs((aptitude * T) - (topSynergy[i].quality * S));
-          topSynergy[i].quality = rank;
+        // Creates a list of schools & sorts them based on rank
+        for (int i = 0; i != 25; ++i) {
+          double newQuality = - (Math.abs((aptitude * T) - (synergy[i].quality * S)));
+          preferences[i] = new School(i, synergy[i].synergy, newQuality);
         }
-        Arrays.sort(topSynergy);
+        Arrays.sort(preferences);
 
         // Sorts the preferences and outputs a list of 10
         int[] ret = new int[10];
         for (int i = 0; i != 10; ++i) {
-          ret[i] = topSynergy[i].index;
+          ret[i] = synergy[i].index;
         }
         return ret;
     }
