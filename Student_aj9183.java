e676f6c0f16995f7e65a9d0b5ee1986a5efc398f
Arnold Jiang <aj9183@princeton.edu>
2024-02-16 22:36:58 -0500

    weird chatbot one that produces ~67%

--- Student_aj9183.java
+++ Student_aj9183.java
@@ -35,20 +35,30 @@ public class Student_aj9183 implements Student {
 
         // Creates a list of preferences for each school
         School[] synergy = new School[schools.size()];
-        School[] preferences = new School[25];
+        School[] preferences = new School[schools.size()];
 
-        // sorts the schools based on synergies
-        for (int i = 0; i != schools.size(); ++i) {
-          synergy[i] = new School(i, synergies.get(i), schools.get(i));
-        }
-        Arrays.sort(synergy);
-
-        // Creates a list of schools & sorts them based on rank
-        for (int i = 0; i != 25; ++i) {
-          double newQuality = - (Math.abs((aptitude * T) - (synergy[i].quality * S)));
-          preferences[i] = new School(i, newQuality);
-        }
+        for (int i = 0; i < schools.size(); i++) {
+          double schoolQuality = schools.get(i) * (1 + aptitude * synergies.get(i));
+          double synergyDistance = Math.abs(synergies.get(i) - aptitude);
+          double weightedScore = S * synergyDistance + W * schoolQuality;
+          preferences[i] = new School(i, weightedScore);
+      }
         Arrays.sort(preferences);
+
+
+
+        // // sorts the schools based on synergies
+        // for (int i = 0; i != schools.size(); ++i) {
+        //   synergy[i] = new School(i, synergies.get(i), schools.get(i));
+        // }
+        // Arrays.sort(synergy);
+
+        // // Creates a list of schools & sorts them based on rank
+        // for (int i = 0; i != 25; ++i) {
+        //   double newQuality = - (Math.abs((aptitude * T) - (synergy[i].quality * S)));
+        //   preferences[i] = new School(i, newQuality);
+        // }
+        // Arrays.sort(preferences);
 
         // Sorts the preferences and outputs a list of 10
         int[] ret = new int[10];
